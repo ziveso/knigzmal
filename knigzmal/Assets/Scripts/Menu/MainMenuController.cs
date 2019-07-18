1ad4958d320bde166dd69607cd04b4f451ca8d02
thitiwat thongbor <thitiwat.thongbor@gmail.com>
2019-07-18 20:21:01 +0300

    Init MainMenu

---
+++ knigzmal/Assets/Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    private void Awake()
+    {
+        SceneController.Instance.LoadScene("GameScene");
+    }
+}
---
+++ knigzmal/Assets/Scripts/Scene/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("More than one scene controller");
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
